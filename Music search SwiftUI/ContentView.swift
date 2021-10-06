f069c522866c7b0a1e437389a3f672ab5fe8f148
Mohamed JABERI <mohamed.jaberi@externe.e-i.com>
2021-10-06 15:15:10 +0200

    Initial Commit

---
+++ Music search SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Music search SwiftUI
+//
+//  Created by eidd5180 on 06/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Music search SwiftUI/Music_search_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_search_SwiftUIApp.swift
+//  Music search SwiftUI
+//
+//  Created by eidd5180 on 06/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Music_search_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
